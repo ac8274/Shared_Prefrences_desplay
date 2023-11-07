cd0fb4f17bc910be32cef0d9ad2f8a8299e9d8ee
Magshimim <ac8274@bs.amalnet.k12.il>
2023-11-07 14:44:48 +0200

    nothing

---
+++ app/src/main/java/com/example/shared_prefrences_desplay/credits.java
@@ -0,0 +1,14 @@
+package com.example.shared_prefrences_desplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);
+    }
+}
